d998ecef056a50c3783bf3f2f52028d72ce14869
Alexandre Beaulieu <alex@segfault.me>
2014-07-08 18:44:41 +0900

    Added test project, basic fixtures and some failing blackbox tests.

---
+++ DebugDiag.Native.Test/TestNativeType32.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DebugDiag.Native.Test
+{
+    /// <summary>
+    /// 32 bit native type tests.
+    /// </summary>
+    [TestClass]
+    public class TestNativeType32
+    {
+        [TestMethod]
+        public void TestPreloadType()
+        {
+            NativeType.Preload("DebugDiag_Native_Test_App!PODType");
+            Assert.IsTrue(NativeType.IsCached("DebugDiag_Native_Test_App!PODType"));
+        }
+
+        [TestMethod]
+        public void TestAtAddressVtableAsString()
+        {
+            var t = NativeType.AtAddress(Fixtures32.VtableAddr);
+            Assert.AreEqual("VirtualTypeDeriv", t.TypeName);
+            Assert.AreEqual("DebugDiag_Native_Test_App", t.ModuleName);
+            Assert.AreEqual("DebugDiag_Native_Test_App!VirtualTypeDeriv", t.QualifiedName);
+        }
+
+        [TestMethod]
+        public void TestAtAddressVtableAsULong()
+        {
+            var t = NativeType.AtAddress(Fixtures32.VtableAddrULong);
+            Assert.AreEqual("VirtualTypeDeriv", t.TypeName);
+            Assert.AreEqual("DebugDiag_Native_Test_App", t.ModuleName);
+            Assert.AreEqual("DebugDiag_Native_Test_App!VirtualTypeDeriv", t.QualifiedName);
+        }
+
+        [TestMethod]
+        public void TestAtAddressNoVtableAsString()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAtAddressNoVtableAsULong()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAtAddressWithRightType()
+        {
+        }
+
+        [TestMethod]
+        public void TestAtAddressWithWrongType()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAtAddressNull()
+        {
+            var t = NativeType.AtAddress(0, null);
+        }
+    }
+}
---
+++ DebugDiag.Native.Test/TestNativeType64.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugDiag.Native.Test
+{
+    /// <summary>
+    /// 64 bit native type tests.
+    /// </summary>
+    class TestNativeType64
+    {
+    }
+}
