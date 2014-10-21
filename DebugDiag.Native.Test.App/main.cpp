de65acd22950fa54cd307d47a282d4316e2448f2
Alexandre Beaulieu <alex@segfault.me>
2014-10-21 20:45:38 +0900

    Test.App ridiculous fixture intensifies.

--- DebugDiag.Native.Test.App/main.cpp
+++ DebugDiag.Native.Test.App/main.cpp
@@ -1,8 +1,18 @@
 // A silly project used for dump fixtures in DebugDiag.Native.Test
 #include <iostream>
+#include <vector>
+#include <map>
 
 struct PODType
 {
+    PODType() : PODType(0,0,0) 
+    {
+    }
+
+    PODType(int o1, int o2, int o3) : Offset1(o1), Offset2(o2), Offset3(o3)
+    {
+    }
+
     int Offset1;
     int Offset2;
     int Offset3;
@@ -58,6 +68,25 @@ int main(int argc, char** argv)
     deriv->OverrideMe(12); // 13
 
     HasAStaticField* statico = new HasAStaticField();
+
+    // Test enumerable with POD.
+    std::vector<PODType> objVector;
+    objVector.push_back(PODType(1,1,1));
+    objVector.push_back(PODType(2, 2, 2));
+    objVector.push_back(PODType(3, 3, 3));
+
+    /// Test enumerable with pointers.
+    std::vector<PODType*> ptrVector;
+    ptrVector.push_back(new PODType(1, 1, 1));
+    ptrVector.push_back(new PODType(2, 2, 2));
+    ptrVector.push_back(new PODType(3, 3, 3));
+
+    // Test map enumerable with primitives
+    std::map<int, int> objMap;
+    objMap[1] = 2;
+    objMap[2] = 4;
+    objMap[3] = 6;
+    objMap[4] = 8;
 
     int* crash = nullptr;
     std::cout << *crash; // segfault
