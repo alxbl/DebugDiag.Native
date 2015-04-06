fd401704f4a1c6b46950691b4f5ac6748d2e03fc
Alexandre Beaulieu <alex@segfault.me>
2015-04-06 13:37:57 +0900

    Added helper script to copy windbg native extensions.

--- DebugDiag.Native.DbgExt/InstallExtensions.bat
+++ DebugDiag.Native.DbgExt/InstallExtensions.bat
@@ -1,4 +1,5 @@
-REM * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+@echo off
+REM r * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 REM This script copies the windbg extensions into the DebugDiag install folder.
 REM
 REM It will check for DebugDiag in the following locations:
@@ -9,19 +10,51 @@ REM     C:\Program Files\DebugDiag\
 REM
 REM If your install folder is not one of the above, you can override the install
 REM folder by setting the DEBUGDIAG environment variable to the proper path.
-REM * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-@echo off
-DEBUGDIAG = "C:\Program Files\DebugDiag\"
+REM r * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+set IMGDIR=%~dp0
+IF NOT "%DEBUGDIAG%"=="" set ENVDD=%DEBUGDIAG%
+
+set DEBUGDIAG=C:\Program Files\DebugDiag
 
 REM 1. Check in Program Files...
-echo Checking for C:\Program Files\DebugDiag...
+echo Checking for %DEBUGDIAG%...
+IF NOT EXIST "%DEBUGDIAG%" GOTO TryX86
+GOTO CopyFiles
 
 
 REM 2. Check in Program Files (x86)
-echo Checking for C:\Program Files (x86)\DebugDiag...
+:TryX86
+set DEBUGDIAG=C:\Program Files (x86)\DebugDiag
+echo Checking for %DEBUGDIAG%...
+IF NOT EXIST "%DEBUGDIAG%" GOTO TryEnv
+GOTO CopyFiles
+
 
 REM 3. Check for user defined path.
-echo Checking for `DEBUGDIAG` environment variable...
+:TryEnv
+set DEBUGDIAG=%ENVDD%
+echo Checking for `DEBUGDIAG` (%DEBUGDIAG%) environment variable...
+set DEBUGDIAG=%ENVDD%
+IF NOT EXIST "%DEBUGDIAG%" GOTO Fail
+GOTO CopyFiles
 
 
+:CopyFiles
+echo Microsoft Debug Diagnostics located at %DEBUGDIAG%.
+echo Copying files from %IMGDIR%...
+
+@echo on
+copy "%IMGDIR%redist\NDbgExt.dll" "%DEBUGDIAG%\x86support\Exts\NDbgExt.dll"
+copy "%IMGDIR%redist\NDbgExt.pdb" "%DEBUGDIAG%\x86support\Exts\NDbgExt.pdb"
+copy "%IMGDIR%redist\NDbgExt64.dll" "%DEBUGDIAG%\Exts\NDbgExt.dll"
+copy "%IMGDIR%redist\NDbgExt64.pdb" "%DEBUGDIAG%\Exts\NDbgExt.pdb"
+@echo off
+
+echo done.
+GOTO Exit
+
+:Fail
 echo Microsoft Debug Diagnostics folder not found... abort.
+
+:Exit
+pause
